aa25e8f215cd1b897affd89d52051018d4ca4cc5
DaryaEgorova23 <dusy230596@yandex.ru>
2023-09-04 19:34:21 +0300

    Добавление файлов

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Дарья Егорова, 8-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://0b448f1e-32e9-4a73-bbfd-413c30f63c1f.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
